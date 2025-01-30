79a6befcb88a616474ecad6f3bda92ea371326e3
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-01-29 23:05:49 -0500

    adjust roles and create middleware for roles

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,28 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/dashboard', function () {
+        return response()->json(['message' => 'Bienvenido al Dashboard']);
+    });
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/admin', function () {
+            return response()->json(['message' => 'Área de Admin']);
+        });
+    });
+
+    Route::middleware(['role:doctor'])->group(function () {
+        Route::get('/doctor', function () {
+            return response()->json(['message' => 'Área de Doctor']);
+        });
+    });
+});
+
+
 Route::resource('users', UserController::class);
 Route::get('/doctors', [DoctorController::class, 'index']);
 Route::get('/appointments/{doctorId}/future', [AppointmentController::class, 'futureAppointments']);
+
+
+
