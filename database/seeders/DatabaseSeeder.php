3c6b39bc44503cf1b07703cb64ef1ee09896c66d
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-02-13 16:58:34 -0500

    remove permission table migration - add role admin for my admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enum\UserRoleEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'lastname' => 'Test Lastname',
             'email' => 'jorgeconsalvacion@gmail.com',
+            'role' => UserRoleEnum::ADMIN->value,
         ]);
 
         $this->call([
