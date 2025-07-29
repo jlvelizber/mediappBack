2dca246cc1830f69bc89c12949e163053d590c53
Jorge Veliz <jorgeconsalvacion@gmail.com>
2025-07-29 18:06:14 -0500

    replace clinical record

--- app/Models/MedicalRecord.php
+++ app/Models/MedicalRecord.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicalRecord extends Model
 {
-    //
+    protected $fillable = [
+        'appointment_id',
+        'symptoms',
+        'diagnosis',
+        'treatment',
+        'notes',
+    ];
+
+    public function appointment()
+    {
+        return $this->belongsTo(Appointment::class);
+    }
+
+    public function prescriptions()
+    {
+        return $this->hasMany(Prescription::class);
+    }
 }
